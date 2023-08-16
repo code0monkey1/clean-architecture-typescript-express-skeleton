44aaab5a55d840e94a6640b6c6be5e0a5d0beba2
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-16 09:47:04 +0000

    16 Aug 2023, 15:17

--- tests/person-validator.test.ts
+++ tests/person-validator.test.ts
@@ -18,14 +18,20 @@ export class PersonValidator implements Validator<Person> {
 export interface ValidationLibrary<T> {
   validate(body: unknown): T | Error;
 }
-
+import { z } from 'zod';
 class ValLibImpl implements ValidationLibrary<Person> {
   validate(body: unknown): Person | Error {
     if (!body) {
       throw new Error('The body is empty');
     }
 
-    return body as Person;
+    const personSchema = z.object({
+      name: z.string().min(3).max(20).trim(),
+    });
+
+    const person = personSchema.parse(body);
+
+    return person;
   }
 }
 
