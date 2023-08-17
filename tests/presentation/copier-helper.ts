4a70337f63b656845a7d2c5c018c6724725a7ddd
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-17 16:29:36 +0000

    17 Aug 2023, 21:59

--- tests/presentation/copier-helper.ts
+++ tests/presentation/copier-helper.ts
@@ -2,15 +2,14 @@ export const copiedChars: string[] = [];
 
 export const readChar = jest.fn();
 
-export const writeChar = jest.fn((c: string) => {
-  copiedChars.push(c);
-});
-
 export const getSource = () => {
   return {
     readChar,
   };
 };
+export const writeChar = jest.fn((c: string) => {
+  copiedChars.push(c);
+});
 
 export const getDestination = () => {
   return {
