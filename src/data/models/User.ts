9e40869432dd0f9b4ae6616f8549b552a915278f
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-17 05:05:34 +0000

    17 Aug 2023, 10:35

--- src/data/models/User.ts
+++ src/data/models/User.ts
@@ -8,13 +8,16 @@ export interface IUser extends Document {
   updated: Date;
 }
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  created: { type: Date, default: Date.now },
-  updated: { type: Date, default: Date.now },
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = model<IUser>('User', userSchema);
 
