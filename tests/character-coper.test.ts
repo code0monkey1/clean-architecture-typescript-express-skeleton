6a799c12b2031977d9161c8226a88ea76aedf939
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-18 07:44:56 +0000

    18 Aug 2023, 13:14

--- tests/character-coper.test.ts
+++ tests/character-coper.test.ts
@@ -5,39 +5,17 @@ import {
   isIntersection,
 } from './presentation/copier-helper';
 describe('character-copier', () => {
-  // beforeEach(() => {
-  //   jest.clearAllMocks();
-  //   clearCopiedCharsArray();
-  // });
+  //[ ] No characters , ending in newline
+  //[ ] Once character , ending in newline
+  //[ ] Two characters , ending in newline
+  //[ ] Many characters, ending in newline
+  //[ ] Order of characters
+  //[ ] Characters after newline should not be written
 
-  // //[ ] No characters , ending in newline
-  // //[ ] Once character , ending in newline
-  // //[ ] Two characters , ending in newline
-  // //[ ] Many characters, ending in newline
-  // //[ ] Order of characters
-  // //[ ] Characters after newline should not be written
-
-  // describe('reads character from source', () => {
-  //   //[+] No characters , ending in newline
-  //   it('does not call the destination method if there is no char is source', () => {
-  //     const src: Source = getSource();
-
-  //     const dest: Destination = getDestination();
-
-  //     readChar.mockReturnValueOnce('\n');
-
-  //     const sut = new Copier(src, dest);
-
-  //     sut.copy();
-
-  //     expect(writeChar).toBeCalledTimes(0);
-  //   });
-
-  //[+] Once character , ending in newline
-  it.each([{ char: 'a' }, { char: 'b' }, { char: 'c' }, { char: 'd' }])(
-    'reads first char from source',
-    ({ char }) => {
-      const src: Source = getSource([char]);
+  describe('reads character from source', () => {
+    //[+] No characters , ending in newline
+    it('does not call the destination method if there is no char is source', () => {
+      const src: Source = getSource([]);
 
       const dest = getDestination();
 
@@ -45,36 +23,75 @@ describe('character-copier', () => {
 
       sut.copy();
 
-      expect(dest.writeChar).toHaveBeenCalledWith(char);
-    }
-  );
-  //[+] Two characters , ending in newline
-  it.each([
-    { chars: ['a', 'b'] },
-    { chars: ['c', 'd'] },
-    { chars: ['e', 'f'] },
-  ])('reads exactly 2 times before encountering a newline', ({ chars }) => {
-    const src: Source = getSource(chars);
+      expect(dest.writeChar).toBeCalledTimes(0);
+    });
 
-    const dest = getDestination();
+    //[+] Once character , ending in newline
+    it.each([{ char: 'a' }, { char: 'b' }, { char: 'c' }, { char: 'd' }])(
+      'reads first char from source',
+      ({ char }) => {
+        const src: Source = getSource([char]);
 
-    expect(dest.writeChar).toHaveBeenCalledWith(chars[0]);
-    expect(dest.writeChar).toHaveBeenCalledWith(chars[1]);
+        const dest = getDestination();
 
-    const sut = new Copier(src, dest);
+        const sut = new Copier(src, dest);
 
-    sut.copy();
+        sut.copy();
 
-    expect(dest.writeChar).toBeCalledTimes(2);
-  });
-  //[+] Many characters, ending in newline
-  it.each([
-    { chars: ['a', 'b', 'c', 'd', 'g', 'c'] },
-    { chars: ['c', 'd', 'e', 'f', 'd', 'f', 'd'] },
-    { chars: ['e', 'f', 'g', 'd', 'd', ''] },
-  ])(
-    'reads exactly $chars.length times before encountering a newline',
-    ({ chars }) => {
+        expect(dest.writeChar).toHaveBeenCalledWith(char);
+      }
+    );
+    //[+] Two characters , ending in newline
+    it.each([
+      { chars: ['a', 'b'] },
+      { chars: ['c', 'd'] },
+      { chars: ['e', 'f'] },
+    ])('reads exactly 2 times before encountering a newline', ({ chars }) => {
+      const src: Source = getSource(chars);
+
+      const dest = getDestination();
+
+      const sut = new Copier(src, dest);
+
+      sut.copy();
+
+      expect(dest.writeChar).toHaveBeenCalledWith(chars[0]);
+      expect(dest.writeChar).toHaveBeenCalledWith(chars[1]);
+      expect(dest.writeChar).toBeCalledTimes(2);
+    });
+    //[+] Many characters, ending in newline
+    it.each([
+      { chars: ['a', 'b', 'c', 'd', 'g', 'c'] },
+      { chars: ['c', 'd', 'e', 'f', 'd', 'f', 'd'] },
+      { chars: ['e', 'f', 'g', 'd', 'd', ''] },
+    ])(
+      'reads exactly $chars.length times before encountering a newline',
+      ({ chars }) => {
+        const src: Source = getSource(chars);
+
+        const dest = getDestination();
+
+        const sut = new Copier(src, dest);
+
+        sut.copy();
+
+        expect(dest.writeChar).toBeCalledTimes(chars.length);
+
+        //multiple characters written (order does not matter)
+        chars.forEach((c) => expect(dest.writeChar).toHaveBeenCalledWith(c));
+
+        // confirm last called character
+        expect(dest.writeChar).toHaveBeenLastCalledWith(
+          chars[chars.length - 1]
+        );
+      }
+    );
+    //[+] Order of characters
+    it.each([
+      { chars: ['a', 'b', 'c', 'd', 'g', 'c'] },
+      { chars: ['c', 'd', 'e', 'f', 'd', 'f', 'd'] },
+      { chars: ['e', 'f', 'g', 'd', 'd', ''] },
+    ])('has all characters copied in teh same order', ({ chars }) => {
       const src: Source = getSource(chars);
 
       const dest = getDestination();
@@ -85,56 +102,34 @@ describe('character-copier', () => {
 
       expect(dest.writeChar).toBeCalledTimes(chars.length);
 
-      //multiple characters written (order does not matter)
-      chars.forEach((c) => expect(dest.writeChar).toHaveBeenCalledWith(c));
+      expect(dest.getWrittenChars()).toEqual(chars);
+    });
+    //[+] Characters after newline should not be written
+    it.each([
+      {
+        chars: [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `\n`, `a`, `b`],
+        before: [`1`, `2`, `3`, `4`, `5`, `6`, `7`],
+        after: [`a`, `b`],
+      },
+      {
+        chars: [`1`, `2`, `7`, `5`, `4`, `\n`, `c`, `d`],
+        before: [`1`, `2`, `7`, `5`, `4`],
+        after: [`c`, `d`],
+      },
+    ])(
+      'has all characters before : $before , the newline  and none after : $after',
+      ({ chars, before, after }) => {
+        const src: Source = getSource(chars);
 
-      // confirm last called character
-      expect(dest.writeChar).toHaveBeenLastCalledWith(chars[chars.length - 1]);
-    }
-  );
-  //[+] Order of characters
-  it.each([
-    { chars: ['a', 'b', 'c', 'd', 'g', 'c'] },
-    { chars: ['c', 'd', 'e', 'f', 'd', 'f', 'd'] },
-    { chars: ['e', 'f', 'g', 'd', 'd', ''] },
-  ])('has all characters copied in teh same order', ({ chars }) => {
-    const src: Source = getSource(chars);
+        const dest = getDestination();
 
-    const dest = getDestination();
+        const sut = new Copier(src, dest);
 
-    const sut = new Copier(src, dest);
+        sut.copy();
 
-    sut.copy();
-
-    expect(dest.writeChar).toBeCalledTimes(chars.length);
-
-    expect(dest.getWrittenChars()).toEqual(chars);
+        expect(dest.getWrittenChars()).toStrictEqual(before);
+        expect(!isIntersection(after, dest.getWrittenChars())).toBe(true);
+      }
+    );
   });
-  //[+] Characters after newline should not be written
-  it.each([
-    {
-      chars: [`1`, `2`, `3`, `4`, `5`, `6`, `7`, `\n`, `a`, `b`],
-      before: [`1`, `2`, `3`, `4`, `5`, `6`, `7`],
-      after: [`a`, `b`],
-    },
-    {
-      chars: [`1`, `2`, `7`, `5`, `4`, `\n`, `c`, `d`],
-      before: [`1`, `2`, `7`, `5`, `4`],
-      after: [`c`, `d`],
-    },
-  ])(
-    'has all characters before : $before , the newline  and none after : $after',
-    ({ chars, before, after }) => {
-      const src: Source = getSource(chars);
-
-      const dest = getDestination();
-
-      const sut = new Copier(src, dest);
-
-      sut.copy();
-
-      expect(dest.getWrittenChars()).toStrictEqual(before);
-      expect(!isIntersection(after, dest.getWrittenChars())).toBe(true);
-    }
-  );
 });
