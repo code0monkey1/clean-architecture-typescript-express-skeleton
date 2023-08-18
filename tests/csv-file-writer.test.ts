8a11150857d063cbadbcc442c9834f7c3a121895
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-18 20:50:13 +0000

    19 Aug 2023, 02:20

--- tests/csv-file-writer.test.ts
+++ tests/csv-file-writer.test.ts
@@ -1,9 +1,23 @@
+import CsvFileWriter from './csv-file-writer';
+import { getFileSystem } from './csv-file-writer.helper';
+
 describe('csv-file-writer', () => {
   //'given : no customer object'
   //'when : we try to write the line to file
   //`then : writeLine function is not called
 
-  it.todo('given : no customer object');
+  it('given : no customer object', () => {
+    //arrange
+    const fs = getFileSystem();
+
+    //act
+    const sut = new CsvFileWriter(fs);
+
+    sut.writeCustomers('myfile.pdf', []);
+
+    //assert
+    expect(fs.writeLine).toBeCalledTimes(0);
+  });
 
   //given : single customer object is present
   //when : we try to write the object info to file
