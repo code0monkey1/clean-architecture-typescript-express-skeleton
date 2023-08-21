c51af2a21f0f24540f58c60f20a9c1ecdcb320fb
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-21 09:41:13 +0000

    21 Aug 2023, 15:11

--- tests/customer-file-writer.ts
+++ tests/customer-file-writer.ts
@@ -2,6 +2,36 @@ import Customer from './customer';
 import FileWriter from './file-writer';
 export class BatchedCustomerFileWriter {
   constructor(private readonly customerFileWriter: CustomerFileWriter) {}
+
+  writeBatchedCustomers(
+    fileName: string,
+    customers: Customer[],
+    batchSize: number
+  ) {
+    let fileIndex = 0;
+
+    while (customers.length) {
+      const customersToWrite = customers.splice(0, batchSize);
+
+      const [name, ext] = this.name_ext(fileName);
+
+      const indexed_fileName = name + fileIndex + ext;
+
+      fileIndex++;
+
+      this.customerFileWriter.writeCustomers(
+        indexed_fileName,
+        customersToWrite
+      );
+    }
+  }
+
+  private name_ext = (file: string) => {
+    const name = file.slice(0, file.lastIndexOf('.'));
+    const ext = file.slice(file.lastIndexOf('.'));
+
+    return [name, ext];
+  };
 }
 export class CustomerFileWriter {
   constructor(private readonly fileWriter: FileWriter) {}
