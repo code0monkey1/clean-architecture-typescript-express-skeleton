bcc34c607376862a4e8ef9bec49d29c342ee7b81
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-23 08:54:00 +0000

    23 Aug 2023, 14:24

--- tests/customer-file-writer.test.ts
+++ tests/customer-file-writer.test.ts
@@ -3,85 +3,85 @@ import {
   assertCustomersHaveBeenWritten,
   batchedCustomersHaveBeenWritten,
   createCustomers,
+  customerToString,
   getBatchedCustomerWriter,
-  // customerToString,
   getCustomer,
   getCustomerFileWriter,
   getFileWriter,
   getUniqueCustomerFileWriter,
 } from './customer-file-writer.helper';
 
-// describe('customer file writer', () => {
-//   //'given : no customer object'
-//   //'when : we try to write the line to file
-//   //`then : writeLine function is not called
+describe('customer file writer', () => {
+  //'given : no customer object'
+  //'when : we try to write the line to file
+  //`then : writeLine function is not called
 
-//   it('no customer object will result in writeLine not being called', () => {
-//     //arrange
-//     const fileWriter = getFileWriter();
-//     const fileName = 'myfile.pdf';
+  it('no customer object will result in writeLine not being called', () => {
+    //arrange
+    const fileWriter = getFileWriter();
+    const fileName = 'myfile.pdf';
 
-//     //act
-//     const sut = getCustomerFileWriter(fileWriter);
+    //act
+    const sut = getCustomerFileWriter(fileWriter);
 
-//     sut.writeCustomers(fileName, []);
+    sut.writeCustomers(fileName, []);
 
-//     //assert
-//     expect(fileWriter.writeLine).toBeCalledTimes(0);
-//   });
+    //assert
+    expect(fileWriter.writeLine).toBeCalledTimes(0);
+  });
 
-//   //given : single customer object is present
-//   //when : we try to write the object info to file
-//   // then : only single object with the given info gets written
-//   it('given : single customer object is present', () => {
-//     //arrange
-//     const fileWriter = getFileWriter();
-//     const customer = getCustomer('Glen', '32');
-//     const fileName = 'myfile.pdf';
+  //   //given : single customer object is present
+  //   //when : we try to write the object info to file
+  //   // then : only single object with the given info gets written
+  it('given : single customer object is present', () => {
+    //arrange
+    const fileWriter = getFileWriter();
+    const customer = getCustomer('Glen', '32');
+    const fileName = 'myfile.pdf';
 
-//     //act
-//     const sut = getCustomerFileWriter(fileWriter);
-//     sut.writeCustomers(fileName, [customer]);
+    //act
+    const sut = getCustomerFileWriter(fileWriter);
+    sut.writeCustomers(fileName, [customer]);
 
-//     //assert
-//     expect(fileWriter.writeLine).toBeCalledTimes(1);
-//     expect(fileWriter.writeLine).toHaveBeenCalledWith(
-//       fileName,
-//       customerToString(customer)
-//     );
-//     expect(fileWriter.getLines()).toStrictEqual([customerToString(customer)]);
-//     // expect(fs.getFileNames().length).toBe(1);
-//     // expect(fs.getLines().length).toBe(1);
-//   });
+    //assert
+    expect(fileWriter.writeLine).toBeCalledTimes(1);
+    expect(fileWriter.writeLine).toHaveBeenCalledWith(
+      fileName,
+      customerToString(customer)
+    );
+    expect(fileWriter.getLines()).toStrictEqual([customerToString(customer)]);
+    // expect(fs.getFileNames().length).toBe(1);
+    // expect(fs.getLines().length).toBe(1);
+  });
 
-//   //given : multiple customer objects are present
-//   //when : we try to write the objects info to file
-//   // then :the exact number and the exact order of customers are written to file
+  //given : multiple customer objects are present
+  //when : we try to write the objects info to file
+  // then :the exact number and the exact order of customers are written to file
 
-//   it.each([
-//     {
-//       customers: createCustomers(3),
-//     },
-//     {
-//       customers: createCustomers(12),
-//     },
-//   ])('given : multiple customers are present', ({ customers }) => {
-//     //arrange
-//     const fileWriter = getFileWriter();
-//     const fileName = 'narnia.ts';
+  it.each([
+    {
+      customers: createCustomers(3),
+    },
+    {
+      customers: createCustomers(12),
+    },
+  ])('given : multiple customers are present', ({ customers }) => {
+    //arrange
+    const fileWriter = getFileWriter();
+    const fileName = 'narnia.ts';
 
-//     //act
-//     const sut = getCustomerFileWriter(fileWriter);
+    //act
+    const sut = getCustomerFileWriter(fileWriter);
 
-//     sut.writeCustomers(fileName, customers);
+    sut.writeCustomers(fileName, customers);
 
-//     //assert
-//     assertCustomersHaveBeenWritten(fileWriter, fileName, customers);
-//     //has same number of items as the number of customers
+    //assert
+    assertCustomersHaveBeenWritten(fileWriter, fileName, customers);
+    //has same number of items as the number of customers
 
-//     expect(fileWriter.getLines().length).toBe(customers.length);
-//   });
-// });
+    expect(fileWriter.getLines().length).toBe(customers.length);
+  });
+});
 
 describe('batched customers', () => {
   it('creates only 1 file, when customers are 12 ', () => {
