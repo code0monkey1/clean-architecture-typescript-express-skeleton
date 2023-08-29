28e4398599513c2f78512c02dda997f2eb24d553
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-29 02:41:46 +0000

    29 Aug 2023, 08:11

--- src/utils/interfaces/validator.ts
+++ src/utils/interfaces/validator.ts
@@ -4,15 +4,29 @@ export type Person = {
   name: string;
 };
 export interface ValidationLibrary<T> {
-  validate(body: unknown): Promise<T | Error>;
+  validate(
+    body: unknown
+  ): Promise<{ success: boolean; errors: unknown; data: T | null }>;
 }
 export class RegisterValidator implements ValidationLibrary<Person> {
-  async validate(body: unknown): Promise<Person | Error> {
-    const personSchema = z.object({
-      name: z.string().min(3).max(20).trim(),
-    });
+  async validate(
+    body: unknown
+  ): Promise<{ success: boolean; errors: unknown; data: Person | null }> {
+    try {
+      const personSchema = z.object({
+        name: z.string().min(3).max(20).trim(),
+      });
 
-    const person = await personSchema.parseAsync(body);
-    return person;
+      const person = await personSchema.parseAsync(body);
+
+      return { success: true, errors: null, data: person };
+    } catch (err: unknown) {
+      let errors = '';
+      if (err instanceof Error) {
+        errors += err.message;
+      }
+
+      return { success: false, errors, data: null };
+    }
   }
 }
