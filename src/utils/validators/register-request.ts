3e2d143eff1fb6156f7e7d235b2422b3ff048c7c
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-08-29 02:19:05 +0000

    29 Aug 2023, 07:49

--- src/utils/validators/register-request.ts
+++ src/utils/validators/register-request.ts
@@ -1,15 +1,15 @@
+import { IUser } from 'data/models/User';
 import { z } from 'zod';
 
-export type Person = {
-  name: string;
-};
+export type User = Pick<IUser, 'name'>;
+
 export interface ValidationLibrary<T> {
   validate(body: unknown): Promise<T | Error>;
 }
-class RegisterRequest implements ValidationLibrary<Person> {
-  async validate(body: unknown): Promise<Person | Error> {
+class RegisterRequest implements ValidationLibrary<User> {
+  async validate(body: unknown): Promise<User | Error> {
     const personSchema = z.object({
-      name: z.string().min(3).max(20).trim(),
+      name: z.string().min(2).max(20).trim(),
     });
 
     const person = await personSchema.parseAsync(body);
